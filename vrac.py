de98b05b18e950be7e1a6e65bd2680fa451a3fdf
Mathieu Barbe <mat.barbe@gmail.com>
2017-05-08 11:41:43 +0200

    ajout de untex et on ne le charge pas

--- vrac.py
+++ vrac.py
@@ -5,5 +5,5 @@ from vraclib import home
 from vraclib import indent
 from vraclib import del_word_backward
 #from vraclib import bip
-from vraclib import move_emacs
-from vraclib import untex
+#from vraclib import move_emacs
+#from vraclib import untex
---
+++ vraclib/untex.py
@@ -0,0 +1,10 @@
+# -*-coding:Utf-8 -*
+
+import sixpad as sp
+page = sp.window.curPage
+
+def untex():
+	# end def
+
+sp.window.addAccelerator(
+		'ALT+F6', untex)
