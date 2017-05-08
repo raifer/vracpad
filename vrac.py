a00c9f84f8c50ab49ef4fd19cbddb1575ce39588
Mathieu Barbe <mat.barbe@gmail.com>
2017-05-08 20:53:07 +0200

    MaJ de vrac.py

--- vrac.py
+++ vrac.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from vraclib import indent
+from vraclib import bip
 from vraclib import del_line
 from vraclib import home
-from vraclib import indent
 from vraclib import del_word_backward
-#from vraclib import bip
 #from vraclib import move_emacs
 #from vraclib import untex
+
+import sixpad as sp
+sp.window.messageBeep(0)
