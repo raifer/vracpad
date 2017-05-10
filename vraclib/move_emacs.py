096329cef89c93d084bb34b90c09a6ddf4c27835
Mathieu Barbe <mat.barbe@gmail.com>
2017-05-10 15:31:52 +0200

    Ajout de move down

--- vraclib/move_emacs.py
+++ vraclib/move_emacs.py
@@ -3,6 +3,7 @@
 import sixpad as sp
 page = sp.window.curPage
 
+
 def move_up():
 	"""Move up one line and read it, 
 	return True if they position has changed, False otherwise"""
@@ -24,5 +25,36 @@ def move_up():
 	return True
 # end def
 
+def move_down():
+	"""Go to the next line and read it, 
+	return True if they position has changed, False otherwise"""
+	
+	line, col = page.licol(page.position)
+	
+	if line == page.lineCount-1:
+		sp.window.messageBeep(0)
+		return False
+	# end if
+	
+	# On regarde si la ligne destination n'est pas plus courte.
+	if page.lineLength(line+1) < col:
+		col = page.lineLength(line+1)
+	# end if
+	
+	page.position = page.licol(line+1, col)	
+	sp.say(page.curLineText, True)
+	return True
+# end def
+
+# Ajout des raccourcis
+# Move up : CTRL P
 sp.window.addAccelerator(
 		'CTRL+p', move_up)
+
+# Move down : CTRL N
+# On modifi le raccourci ctrl + n de nouveau en ctrl + t
+item_new = sp.window.menus['file'][0]['new']
+item_new.accelerator = "CTRL+t"
+# Ajout accelerator move down
+sp.window.addAccelerator(
+		'CTRL+n', move_down)
