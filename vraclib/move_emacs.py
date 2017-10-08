0c32c8f2f8e4ed816a1a897b284bd923579a993b
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-08 15:32:24 +0200

    Ajout de la touche CTRL+a

--- vraclib/move_emacs.py
+++ vraclib/move_emacs.py
@@ -48,6 +48,30 @@ def move_down():
 	return True
 # end def
 
+def move_start():
+
+	page = sp.window.curPage
+	#On vérifie si la ligne commence directement avec du texte
+	if page.lineStartOffset(page.curLine) == page.lineSafeStartOffset(page.curLine):
+		page.position = page.lineStartOffset(page.curLine)
+		#sp.window.beep(220, 100)
+		#sp.window.beep(220, 100)
+	# On vérifie si le curseur est au début du texte
+	elif page.position == page.lineSafeStartOffset(page.curLine):
+		# On se positionne au début de la ligne sur n'importe quel caractère
+		page.position = page.lineStartOffset(page.curLine)
+		#sp.window.beep(220, 100)
+	else:
+		# On se positionne sur le premier caractère qui n'est pas une espace ou une tabulation
+		page.position = page.lineSafeStartOffset(page.curLine)
+		#sp.window.beep(330, 100)
+		#sp.window.beep(440, 100)
+	if (page.textLength) : sp.say(page.text[page.position], True)
+	else : sp.say("vide");
+
+
+
+# Main
 # Ajout des raccourcis
 # Move up : CTRL P
 sp.window.addAccelerator(
@@ -57,6 +81,17 @@ sp.window.addAccelerator(
 # On modifi le raccourci ctrl + n de nouveau en ctrl + t
 item_new = sp.window.menus['file'][0]['new']
 item_new.accelerator = "CTRL+t"
+
 # Ajout accelerator move down
 sp.window.addAccelerator(
 		'CTRL+n', move_down)
+
+# Move start : CTRL P
+sp.window.addAccelerator(
+		'CTRL+a', move_start)
+
+# On modifi le raccourci ctrl + a
+item_select_all = sp.window.menus['edit']['selectAll'] #['select all']
+item_select_all.accelerator = "ALT+a"
+
+
